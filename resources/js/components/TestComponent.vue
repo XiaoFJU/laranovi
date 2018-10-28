3afcad0e3e4c72f023f14cab63f03d5c4a6baf8b
Xiao Xiao <xiao.xiao@tenoz.tw>
2018-10-29 02:46:24 +0800

    frontend

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6" v-for="value in cards">
+                <div class="card card-default">
+                    <div class="card-header">Test Component</div>
+
+                    <div class="card-body">
+                        This is {{ prefixCard(value) }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            // 一進來要做的事情
+            console.log('Component mounted.')
+        },
+        data() {
+            // 定義會用到的資料們
+            return {
+                cards: ['cardA', 'meow', 'neko', 'gopher']
+            }
+        },
+        methods: {
+            // 定義會用到的 function們
+            prefixCard(cardName) {
+                return `Oo.${cardName}.oO`
+            }
+        }
+    }
+</script>
---
+++ resources/views/xiao.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- title -->
+    <title>Hello World</title>
+</head>
+<body>
+    <div id="app">
+        <h1>ヽ(✿ﾟ▽ﾟ)ノ ~~ Hello</h1>
+        <test-component></test-component>
+    </div>
+</body>
+</html>
